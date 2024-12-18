e47b3b1db63f9e29c361a3a3a077f0f98a3b22b1
Pasan-FDo <pasanfernando.work@gmail.com>
2024-12-18 15:57:35 +0530

    router authenticated

--- routes/ManageOfficers.js
+++ routes/ManageOfficers.js
@@ -1,46 +1,55 @@
 const express = require('express');
-const ManageOfficerEP = require('../end-point/ManageOfficers-ep')
+const ManageOfficerEP = require('../end-point/ManageOfficers-ep');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 
 router.get(
     "/get-all-center",
+    authMiddleware,
     ManageOfficerEP.getAllCollectionCenter
 )
 
 router.get(
     "/get-last-emp-id/:role",
+    authMiddleware,
     ManageOfficerEP.getForCreateId
 )
 
 router.post(
     "/create-officer",
+    authMiddleware,
     ManageOfficerEP.createOfficer
 )
 
 router.get(
     "/get-by-managerID/:centerId",
+    authMiddleware,
     ManageOfficerEP.getManagerIdByCenterId
 )
 
 router.get(
     "/get-all-officers",
+    authMiddleware,
     ManageOfficerEP.getAllOfficers
 )
 
 router.get(
     "/get-all-company-names",
+    authMiddleware,
     ManageOfficerEP.getAllCompanyNames
 )
 
 router.delete(
     "/delete-officer/:id",
+    authMiddleware,
     ManageOfficerEP.deleteOfficer
 )
 
 router.get(
     "/update-status/:id/:status",
+    authMiddleware,
     ManageOfficerEP.UpdateStatusAndSendPassword
 )
 
