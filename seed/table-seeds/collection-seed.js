9e207a5d742d7a864b6998e6c80fde7038acb49f
Pasan-FDo <pasanfernando.work@gmail.com>
2025-01-07 14:51:50 +0530

    add daily target and daily target items table creation functionality

--- seed/table-seeds/collection-seed.js
+++ seed/table-seeds/collection-seed.js
@@ -1,4 +1,4 @@
-const { createXlsxHistoryTable } = require('../tables/collection-table');
+const { createXlsxHistoryTable, createDailyTargetTable, createDailyTargetItemsTable } = require('../tables/collection-table');
 const { createMarketPriceTable } = require('../tables/collection-table');
 const { createMarketPriceServeTable } = require('../tables/collection-table');
 const { createCompany } = require('../tables/collection-table');
@@ -47,6 +47,11 @@ const seedCollection = async () => {
     const messageCreateMarketPriceRequestTable = await createMarketPriceRequestTable();
     console.log(messageCreateMarketPriceRequestTable);
 
+    const messageCreateDailyTartget = await createDailyTargetTable();
+    console.log(messageCreateDailyTartget);
+
+    const messageCreateDailyTartgetItems = await createDailyTargetItemsTablen();
+    console.log(messageCreateDailyTartgetItems);
 
 
 
