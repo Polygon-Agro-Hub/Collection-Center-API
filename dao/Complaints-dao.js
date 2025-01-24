de90c5e5cec788136abf77e819a8903e3331c8f0
Pasan-FDo <pasanfernando.work@gmail.com>
2025-01-24 16:27:59 +0530

    feat: update complaint DAO to modify complaint status and assignment values

--- dao/Complaints-dao.js
+++ dao/Complaints-dao.js
@@ -198,7 +198,6 @@ exports.getAllSendComplainDao = (userId,companyId, page, limit, status, emptype,
 
 exports.addComplaintDao = (officerId, category, complaint) => {
     return new Promise((resolve, reject) => {
-        // Generate the refNo with the format CC+YYMMDD+000N
         const currentDate = new Date();
         const year = currentDate.getFullYear().toString().slice(-2); // Last two digits of the year
         const month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); // Month as two digits
@@ -224,8 +223,8 @@ exports.addComplaintDao = (officerId, category, complaint) => {
 
             const refNo = `${refNoPrefix}${nextSequence.toString().padStart(4, '0')}`; // Generate refNo
             const language = "English";
-            const status = "Pending";
-            const complainAssign = "Assign";
+            const status = "Assigned";
+            const complainAssign = "CCH";
 
             const sqlInsert = `
                 INSERT INTO officercomplains (officerId, refNo, language, complainCategory, complain, reply, status, complainAssign)
