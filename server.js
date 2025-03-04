f325b6bbe1ceebf0a82d9da6b6feace1efbfba3b
Kavintha Kulasingham <kmuthisha@gmail.com>
2025-03-04 22:16:51 +0530

    Docker changes and base path added

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use(`${BASE_API_PATH}/price-list`, PriceListRoutes);
 app.use(`${BASE_API_PATH}/report`, ReportRoutes);
 app.use(`${BASE_API_PATH}/target`, TargetRoutes);
 app.use(`${BASE_API_PATH}/complaint`, ComplaintRoutes);
-app.use(`${BASE_API_PATH}/health`, heathRoutes);
+app.use(`${BASE_API_PATH}`, heathRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
