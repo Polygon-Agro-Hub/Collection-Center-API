4253c40519e9c4a800a7362098a7f8592966f005
Pasan-FDo <pasanfernando.work@gmail.com>
2025-03-17 10:44:47 +0530

    convert to connection to pool connection

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const {  plantcare, collectionofficer, marketPlace, dash } = require('./startup/database');
+const { admin, plantcare, collectionofficer, marketPlace, dash } = require('./startup/database');
 
 // Base API path constant
 const BASE_API_PATH = '/agro-api/collection-center-api';
 
 //routers
 const AuthRoutes = require('./routes/Auth');
-const ManageOffcerRoutes  = require('./routes/ManageOfficers');
+const ManageOffcerRoutes = require('./routes/ManageOfficers');
 const PriceListRoutes = require('./routes/PriceList');
 const ReportRoutes = require('./routes/Report');
-const TargetRoutes  =require('./routes/Target');
+const TargetRoutes = require('./routes/Target');
 const ComplaintRoutes = require('./routes/Complaints')
 const DashbordRoutes = require('./routes/Dashboards')
 const heathRoutes = require('./routes/healthRoutes')
@@ -20,48 +20,63 @@ const heathRoutes = require('./routes/healthRoutes')
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors({
-    origin: '*'
+  origin: '*'
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-plantcare.connect(err => {
-    if (err) {
-      console.error('Error connecting to the database in index.js (plantcare):', err);
-      return;
-    }
-    console.log('Connected to the MySQL database in server.js.(plantcare)');
-  });
-  
-  collectionofficer.connect(err => {
-    if (err) {
-      console.error('Error connecting to the database in index.js (collectionofficer):', err);
-      return;
-    }
-    console.log('Connected to the MySQL database in server.js.(collectionofficer)');
-  });
-  
-  marketPlace.connect(err => {
-    if (err) {
-      console.error('Error connecting to the database in index.js (marketPlace):', err);
-      return;
-    }
-    console.log('Connected to the MySQL database in server.js.(marketPlace)');
-  });
-  
-  dash.connect(err => {
-    if (err) {
-      console.error('Error connecting to the database in index.js (dash):', err);
-      return;
-    }
-    console.log('Connected to the MySQL database in server.js.(dash)');
-  });
+
+//DB connections
+admin.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to the database in index.js (admin):', err);
+    return;
+  }
+  console.log('Connected to the MySQL database in server.js (admin).');
+  connection.release();
+});
+
+plantcare.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to the database in index.js (plantcare):', err);
+    return;
+  }
+  console.log('Connected to the MySQL database in server.js (plantcare).');
+  connection.release();
+});
+
+collectionofficer.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to the database in index.js (collectionofficer):', err);
+    return;
+  }
+  console.log('Connected to the MySQL database in server.js.(collectionofficer)');
+  connection.release();
+});
+
+marketPlace.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to the database in index.js (marketPlace):', err);
+    return;
+  }
+  console.log('Connected to the MySQL database in server.js.(marketPlace)');
+  connection.release();
+});
+
+dash.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to the database in index.js (dash):', err);
+    return;
+  }
+  console.log('Connected to the MySQL database in server.js.(dash)');
+  connection.release();
+});
 
 
 
 
 app.use(`${BASE_API_PATH}/test`, (req, res) => {
-    res.json("Testing run!")
+  res.json("Testing run!")
 })
 app.use(`${BASE_API_PATH}/api/auth`, AuthRoutes);
 app.use(`${BASE_API_PATH}/api/manage-officers`, ManageOffcerRoutes);
@@ -76,7 +91,7 @@ app.use(`${BASE_API_PATH}`, heathRoutes);
 
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
 
 
--- startup/database.js
+++ startup/database.js
@@ -2,39 +2,77 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 
-const plantcare = mysql.createConnection({
+const admin = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME_AD,
+  charset: 'utf8mb4',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 6, 
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay : 0,
+});
+
+const plantcare = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME_PC,
-  charset: 'utf8mb4'
+  charset: 'utf8mb4',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 6, 
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay : 0,
 });
 
 
-const collectionofficer = mysql.createConnection({
+const collectionofficer = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME_CO,
-  charset: 'utf8mb4'
+  charset: 'utf8mb4',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 6, 
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay : 0,
 });
 
-const marketPlace = mysql.createConnection({
+const marketPlace = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME_MP,
-  charset: 'utf8mb4'
+  charset: 'utf8mb4',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 6, 
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay : 0,
 });
 
 
-const dash = mysql.createConnection({
+const dash = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME_DS,
-  charset: 'utf8mb4'
+  charset: 'utf8mb4',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 6, 
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay : 0,
 });
 
 
-module.exports = {plantcare, collectionofficer, marketPlace, dash};
+module.exports = {admin, plantcare, collectionofficer, marketPlace, dash};
