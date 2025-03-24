70e987efe27fef8a74e3b43eb894d99135d63367
GMchalana <gmchalanaprabhashwara@gmail.com>
2025-03-24 18:14:07 +0530

    add regCode to center name and officer count query

--- dao/Target-dao.js
+++ dao/Target-dao.js
@@ -427,7 +427,7 @@ exports.getOfficerDetailsDAO = (centerId, page, limit, role, status, searchText)
 exports.getCenterNameAndOficerCountDao = (centerId) => {
     return new Promise((resolve, reject) => {
         const sql = `
-           SELECT CC.id, CC.centerName, COUNT(COF.id) AS officerCount
+           SELECT CC.id, CC.centerName, CC.regCode, COUNT(COF.id) AS officerCount
            FROM collectioncenter CC, collectionofficer COF
            WHERE CC.id = ? AND CC.id = COF.centerId
            GROUP BY CC.id, CC.centerName
