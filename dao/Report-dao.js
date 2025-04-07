690bd7d4d99b88939a62de447f63c0f3ae64a45d
AshanIsuranga <21it0478@itum.mrt.ac.lk>
2025-04-07 11:56:10 +0530

    fix download payment sheet

--- dao/Report-dao.js
+++ dao/Report-dao.js
@@ -354,10 +354,10 @@ exports.getAllPaymentsDAO = (companyId, page, limit, searchText, center, date, m
         let countSql = `
             SELECT COUNT(*) AS total
             FROM collection_officer.registeredfarmerpayments rfp
-            JOIN collection_officer.farmerpaymentscrops fpc ON rfp.id = fpc.registerFarmerId
-            JOIN collection_officer.collectionofficer co ON co.id = rfp.collectionOfficerId
-            JOIN collection_officer.collectioncenter cc ON cc.id = co.centerId
-            JOIN plant_care.users u ON u.id = rfp.userId
+            LEFT JOIN collection_officer.farmerpaymentscrops fpc ON rfp.id = fpc.registerFarmerId
+            LEFT JOIN collection_officer.collectionofficer co ON co.id = rfp.collectionOfficerId
+            LEFT JOIN collection_officer.collectioncenter cc ON cc.id = co.centerId
+            LEFT JOIN plant_care.users u ON u.id = rfp.userId
             WHERE co.companyId = ?
         `;
 
@@ -381,10 +381,10 @@ exports.getAllPaymentsDAO = (companyId, page, limit, searchText, center, date, m
                 IFNULL(fpc.gradeCprice, 0) * IFNULL(fpc.gradeCquan, 0)
                 ) AS totalAmount
             FROM collection_officer.registeredfarmerpayments rfp
-            JOIN collection_officer.farmerpaymentscrops fpc ON rfp.id = fpc.registerFarmerId
-            JOIN collection_officer.collectionofficer co ON co.id = rfp.collectionOfficerId
-            JOIN collection_officer.collectioncenter cc ON cc.id = co.centerId
-            JOIN plant_care.users u ON u.id = rfp.userId
+            LEFT JOIN collection_officer.farmerpaymentscrops fpc ON rfp.id = fpc.registerFarmerId
+            LEFT JOIN collection_officer.collectionofficer co ON co.id = rfp.collectionOfficerId
+            LEFT JOIN collection_officer.collectioncenter cc ON cc.id = co.centerId
+            LEFT JOIN plant_care.users u ON u.id = rfp.userId
             WHERE co.companyId = ?
     
 
@@ -476,4 +476,100 @@ exports.getAllPaymentsDAO = (companyId, page, limit, searchText, center, date, m
         });
     });
 };
+
+exports.downloadPaymentReport = (centerId, monthNumber, createdDate, search) => {
+    return new Promise((resolve, reject) => {
+    
+  
+      const params = [];
+      const countParams = [];
+      const totalParams = [];
+  
+      
+      let whereClause = "WHERE c.id = 1";
+  
+      if (centerId) {
+        whereClause += " AND co.centerId = ?";
+        params.push(centerId);
+        countParams.push(centerId);
+        totalParams.push(centerId);
+      }
+  
+      if (monthNumber) {
+        whereClause += " AND MONTH(rfp.createdAt) = ?";
+        params.push(monthNumber);
+        countParams.push(monthNumber);
+        totalParams.push(monthNumber);
+      }
+  
+      if (createdDate) {
+        whereClause += " AND DATE(rfp.createdAt) = ?";
+        params.push(createdDate);
+        countParams.push(createdDate);
+        totalParams.push(createdDate);
+      }
+  
+      if (search) {
+        whereClause += `
+          AND (
+            cc.regCode LIKE ? OR 
+            cc.centerName LIKE ? OR 
+            us.NICnumber LIKE ? OR 
+            invNo LIKE ?
+          )
+        `;
+        const searchPattern = `%${search}%`;
+        params.push(searchPattern, searchPattern, searchPattern, searchPattern);
+        countParams.push(searchPattern, searchPattern, searchPattern, searchPattern);
+        totalParams.push(searchPattern, searchPattern, searchPattern, searchPattern);
+      }
+  
+      console.log('this is', countParams);
+  
+     
+      let dataSql = `
+        SELECT 
+          invNo AS grnNumber,
+          cc.regCode AS regCode,
+          cc.centerName AS centerName,
+          ROUND(SUM(IFNULL(fpc.gradeAprice * fpc.gradeAquan, 0) + IFNULL(fpc.gradeBprice * fpc.gradeBquan, 0) + IFNULL(fpc.gradeCprice * fpc.gradeCquan, 0)), 2) AS amount,
+          us.firstName AS firstName,
+          us.lastName AS lastName,
+          us.NICnumber AS nic,
+          us.phoneNumber AS phoneNumber,
+          us.phoneNumber AS phoneNumber,
+          ub.accHolderName AS accHolderName,
+          ub.accNumber AS accNumber,
+          ub.bankName AS bankName,
+          ub.branchName AS branchName,
+          co.empId AS empId,
+          TIME(rfp.createdAt) AS createdAt
+        FROM 
+          registeredfarmerpayments rfp
+        LEFT JOIN 
+          farmerpaymentscrops fpc ON rfp.id = fpc.registerFarmerId
+        JOIN 
+          collectionofficer co ON rfp.collectionOfficerId = co.id
+        JOIN 
+          plant_care.users us ON rfp.userId = us.id
+        JOIN 
+          collectioncenter cc ON co.centerId = cc.id
+        JOIN 
+          company c ON co.companyId = c.id
+        LEFT JOIN 
+          plant_care.userbankdetails ub ON us.id = ub.userId
+        ${whereClause}
+        GROUP BY rfp.id
+      `;
+  
+      console.log('Executing Count Query...');
+  
+      collectionofficer.query(dataSql, params, (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(results);
+      });
+    });
+  };
 
--- routes/Report.js
+++ routes/Report.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/authMiddleware');
 const ReporttEP = require('../end-point/Report-ep')
+reportDao = require('../dao/Report-dao');
+const XLSX = require('xlsx');
 
 const router = express.Router();
 
@@ -45,6 +47,80 @@ router.get(
     authMiddleware,
     ReporttEP.getAllPayments
 )
+
+router.get('/download-payment-report', async (req, res) => {
+    try {
+
+      console.log(req.query);
+
+      const {centerId, monthNumber, createdDate, search} = req.query;
+      console.log({centerId, monthNumber, createdDate, search});
+      // Fetch data from the database
+      const data = await reportDao.downloadPaymentReport( 
+        centerId,
+        monthNumber,
+        createdDate, 
+        search);
+  
+      // Format data for Excel
+      const formattedData = data.flatMap(item => [
+        {
+          'GRN': item.grnNumber,
+          'Amount': item.amount,
+          'Center Reg Code': item.regCode,
+          'Center Name': item.centerName,
+          'Farmer NIC': item.nic,
+          'Farmer Name': item.firstName + ' ' + item.lastName,
+          'Farmer contact': item.phoneNumber,
+          'Account holder name': item.accHolderName,
+          'Account Number': item.accNumber,
+          'Bank Name': item.bankName,
+          'Branch Name': item.branchName,
+          'Officer EMP ID': item.empId,
+          'Collected time': item.createdAt
+
+        },
+        
+      ]);
+      
+  
+      // Create a worksheet and workbook
+      const worksheet = XLSX.utils.json_to_sheet(formattedData);
+
+      worksheet['!cols'] = [
+        { wch: 25 }, // GRN
+        { wch: 15}, // Amount
+        { wch: 20 }, // Center Reg Code
+        { wch: 25 }, // Center Name
+        { wch: 18 }, // Farmer NIC
+        { wch: 25 }, // Farmer Name
+        { wch: 15 }, // Farmer Contact
+        { wch: 25 }, // Account Holder Name
+        { wch: 20 }, // Account Number
+        { wch: 20 }, // Bank Name
+        { wch: 20 }, // Branch Name
+        { wch: 15 }, // Officer EMP ID
+        { wch: 15 }  // Collected Time
+      ];
+
+
+      const workbook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(workbook, worksheet, 'Farmer Payement Template');
+  
+      // Write the workbook to a buffer
+      const excelBuffer = XLSX.write(workbook, { type: 'buffer', bookType: 'xlsx' });
+  
+      // Set headers for file download
+      res.setHeader('Content-Disposition', 'attachment; filename="Farmer Payement Template.xlsx"');
+      res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+      
+      // Send the file to the client
+      res.send(excelBuffer);
+    } catch (err) {
+      console.error('Error generating Excel file:', err);
+      res.status(500).send('An error occurred while generating the file.');
+    }
+  });
 
 
 
